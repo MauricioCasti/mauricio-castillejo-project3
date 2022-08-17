0b2720255d24cf2b1d75c92a995f5c1bd5e9e642
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-08-17 15:41:58 -0700

    finished and added final touches

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ var timeStart = null;
 var timeRun;
 var mistakes = 0;
 var userInput= '';
-
+var totalTime;
+var scoreBoard = [undefined,undefined,undefined];
+console.log(scoreBoard + " from global method");
+console.log(scoreBoard.length);
+//the quick brown fox jumped over the lazy dog
 
 //Making multiple typing tests
 var tests = {text:["the quick brown fox jumped over the lazy dog",
-            "the course, comp 484, is a class in which you can learn about web design and the code that goes along with it such as HTML, CSS, and Javascript",
+            "the course, comp 484, is a class in which you can learn about HTML, CSS, and Javascript for web design",
             "those crazy contraptions can't create the kind of classic catastrophe one can cause by cleverly concealing a calamitous crater!",]}
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
@@ -27,26 +31,30 @@ function NumberSetup(num){
 // Run a standard minute/second/hundredths timer:
 function timerUpdate(){
     var presentTime = new Date();
-    var differnce = presentTime.getTime() - timeStart.getTime();
+    var difference = presentTime.getTime() - timeStart.getTime();
 
     //makes time into seconds since 1 sec is 1000 milliseconds
-    differnce = differnce/1000;
+    difference = difference/1000;
 
-    if(differnce/60 > 1){
-        var min = Math.floor(differnce/60);
-        var sec = differnce - (min * 60);
+    if(difference/60 > 1){
+        var min = Math.floor(difference/60);
+        var sec = difference - (min * 60);
         document.getElementById("timer").innerHTML = ((min<10) ? NumberSetup(min):min) + ":" + ((sec < 10) ? NumberSetup(sec.toFixed(3)):sec.toFixed(3));
     }else{
-        document.getElementById("timer").innerHTML = "00:" + ((differnce<10) ?NumberSetup(differnce.toFixed(3)) : differnce.toFixed(3));
+        document.getElementById("timer").innerHTML = "00:" + ((difference<10) ?NumberSetup(difference.toFixed(3)) : difference.toFixed(3));
     }
+    // sets total time to show final score
+    totalTime = difference;
 }
 
 // Match the text entered with the provided text on the page:
 function textCheck(){
     // var selected = document.getElementById('type-test').value;
     var selected = getTest();
-    // console.log("entered textCheck")
+    // console.log("entered textCheck");
     // console.log(selected);
+    // learned about undefined when getting values from a event call 
+    //https://stackoverflow.com/questions/2778901/how-to-compare-variables-to-undefined-if-i-don-t-know-whether-they-exist
     if (selected == undefined){
         document.getElementById("type-test").innerHTML = "Test not chosen please select a test.";
         return console.log("Error: Not Selected a Test");
@@ -76,6 +84,8 @@ function textCheck(){
         document.getElementById("border-color").setAttribute("style", "border: 10px solid green");
         document.getElementById("mistakes").innerHTML = "Finished! you made " + mistakes + " mistake(s)";
         document.getElementById("select").setAttribute("style","display: block");
+        //call display score to populate top 3
+        displayScore(totalTime);
     }
 
 }
@@ -95,6 +105,8 @@ function reset(){
     document.getElementById("mistakes").innerHTML = "";
     document.getElementById("test-area").value = '';
     document.getElementById("timer").innerHTML = "00:00:00";
+    document.getElementById("score-board").innerHTML = "";
+    console.log(scoreBoard + " from reset function");
 }
 
 // new handler, relies on dropdown and select button to obtain and display the chosen test
@@ -127,3 +139,29 @@ function getTest(){
 } 
 
 // Event listeners for keyboard input and the reset button:
+
+function displayScore(time){
+    console.log("checking times...");
+
+    //set first by default
+    if(scoreBoard[0] == undefined){
+        scoreBoard[0] = time;
+    }
+    
+    if(time < scoreBoard[0]){ //if new time beats first
+        scoreBoard[2] = scoreBoard[1];
+        scoreBoard[1] = scoreBoard[0];
+        scoreBoard[0] = time;
+    }else if(time > scoreBoard[0] && (time< scoreBoard[1] || scoreBoard[1] == undefined)){ //if new time isnt first but beats second
+        scoreBoard[2] = scoreBoard[1];
+        scoreBoard[1] = time;
+    }else if(time > scoreBoard[1] && (time < scoreBoard[2] || scoreBoard[2] == undefined)){ //if new time isnt second but beats third
+        scoreBoard[2] = time;
+    }else{ //was not fast enough to be tope 3
+        console.log("time not fast for top 3 ranking");
+    }
+
+
+
+    document.getElementById("score-board").innerHTML = "Leaderboard:<br>"+ "1st: " + scoreBoard[0]+ "<br>" + "2nd: " + scoreBoard[1]+ "<br>" + "3rd: " + scoreBoard[2];
+}
