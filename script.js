4a647cf57a19bbb45728f6a4267b0de17064b041
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-08-16 23:03:40 -0700

    selection tests work, condition if you type on a not chosen test, mistakes total shown, final task is displaying leaderboard

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var timeRun;
 var mistakes = 0;
 var userInput= '';
 
+
 //Making multiple typing tests
 var tests = {text:["the quick brown fox jumped over the lazy dog",
             "the course, comp 484, is a class in which you can learn about web design and the code that goes along with it such as HTML, CSS, and Javascript",
@@ -42,13 +43,24 @@ function timerUpdate(){
 
 // Match the text entered with the provided text on the page:
 function textCheck(){
-    if(timeStart == null){
-        startTime();
-        console.log("timer has begun...");
+    // var selected = document.getElementById('type-test').value;
+    var selected = getTest();
+    // console.log("entered textCheck")
+    // console.log(selected);
+    if (selected == undefined){
+        document.getElementById("type-test").innerHTML = "Test not chosen please select a test.";
+        return console.log("Error: Not Selected a Test");
+    }else{
+        if(timeStart == null){
+            startTime();
+            console.log("timer has begun...");
+            document.getElementById("select").setAttribute("style","display: none");
+        }
     }
-    var selected = document.getElementById('type-test');
     userInput = document.getElementById("test-area").value;
 
+    // used a style.remove property to change the style of the selected element
+    // source: https://stackoverflow.com/questions/18691655/remove-style-on-element
     if(userInput.charAt(userInput.length-1) !== selected.charAt(userInput.length-1)){
         document.getElementById("border-color").style.removeProperty("border");
         document.getElementById("border-color").setAttribute("style", "border: 10px solid #ff000099");//removes and re-sets the border property to red if matching is incorrect
@@ -62,7 +74,8 @@ function textCheck(){
     if(userInput == selected){
         clearInterval(timeRun);
         document.getElementById("border-color").setAttribute("style", "border: 10px solid green");
-        console.log("finished! you made " + mistakes + " mistakes");
+        document.getElementById("mistakes").innerHTML = "Finished! you made " + mistakes + " mistake(s)";
+        document.getElementById("select").setAttribute("style","display: block");
     }
 
 }
@@ -78,25 +91,38 @@ function reset(){
     console.log("stopping and resetting");
     clearInterval(timeRun);
     timeStart = null;
-
+    document.getElementById("select").setAttribute("style","display: block");
+    document.getElementById("mistakes").innerHTML = "";
     document.getElementById("test-area").value = '';
     document.getElementById("timer").innerHTML = "00:00:00";
 }
 
-// new addition, relies on dropdown and select button to obtain and display the chosen test
-function displayCurrent(){
-    console.log(selected);
-    var selected = document.getElementById("selection").value;
-    switch(selected){
-        case tests.text[0]:
+// new handler, relies on dropdown and select button to obtain and display the chosen test
+function getTest(){
+    // console.log(document.getElementById("select").value);
+    var choice=document.getElementById("select").value;
+    document.getElementById("mistakes").innerHTML = "";
+    //document.getElementById("selection").value
+    switch(choice){
+        case "test1":
+            var selected = tests.text[0]
+            console.log(selected);
             document.getElementById("type-test").innerHTML = selected;
-            break;
-        case tests.text[1]:
+            return selected;
+        case "test2":
+            var selected = tests.text[1]
+            console.log(selected);
             document.getElementById("type-test").innerHTML = selected;
-            break;
-        case tests.text[2]:
+            return selected;
+        case "test3":
+            var selected = tests.text[2]
+            console.log(selected);
             document.getElementById("type-test").innerHTML = selected;
-            break;
+            return selected;
+            case "null":
+            var selected = null;
+            document.getElementById("type-test").innerHTML = "Please choose a test before starting";
+            return selected;
     }
 } 
 
